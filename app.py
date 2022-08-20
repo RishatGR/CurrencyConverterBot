9e6d035f93dcda83d9569117aa90bc0c66f02fcb
RishatGR <ural.surgut.galiev@gmail.com>
2022-08-20 17:52:56 +0500

    add api json

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import requests
 import telebot
+import json
+
 
 
 TOKEN = '5714731812:AAGDG6nJWBrGXyFxjWH2T8p4Uh3wrvE4j0s'
@@ -7,7 +10,7 @@ TOKEN = '5714731812:AAGDG6nJWBrGXyFxjWH2T8p4Uh3wrvE4j0s'
 bot = telebot.TeleBot(TOKEN)
 
 keys = {
-    'рубрь': 'RUB',
+    'рубль': 'RUB',
     'доллар': 'USD',
     'евро': 'EUR',
 }
@@ -27,6 +30,15 @@ def values(message: telebot.types.Message):
     for key in keys.keys():
         text = '\n'.join((text, key,))
     bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    quote, base, amount = message.text.split(' ')
+    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={keys[quote]}&tsyms={keys[base]}')
+    total_base = json.loads(r.content)[keys[base]]
+    text = f'Цена {amount} {quote} в {base} - {total_base}'
+    bot.send_message(message.chat.id, text)
 
 
 bot.polling()
